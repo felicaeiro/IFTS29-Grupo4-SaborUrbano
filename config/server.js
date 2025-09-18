f46089ab8dac64893517e7b5bd8452ce7e62e38b
Ian Vázquez <cam.iva@hotmail.com>
2025-09-18 20:31:29 -0300

    views - pug

--- config/server.js
+++ config/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const productoRoutes = require('../routes/productoRoutes');
 const pedidoRoutes = require('../routes/pedidoRoutes');
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, '../views'));
 app.use(express.static('public'));
 
 
@@ -13,6 +15,7 @@ app.get('/', (req, res) => {
   res.send('Sabor Urbano - App funcionando');
 });
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // ROUTES
