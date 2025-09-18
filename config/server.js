79a1315d71d57734dccf0da6f1cfb28d6086a424
Feli Caeiro <felicaeiro2@gmail.com>
2025-09-18 20:36:11 -0300

    pug

--- config/server.js
+++ config/server.js
@@ -1,20 +1,26 @@
 const express = require('express');
 const path = require('path');
-
 const productoRoutes = require('../routes/productoRoutes');
 const pedidoRoutes = require('../routes/pedidoRoutes');
+const { fileURLToPath } = require('url');
+const methodOverride = require('method-override');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
 app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '..', 'views'));
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.send('Sabor Urbano - App funcionando');
+  res.render('index');
 });
 
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 app.use(express.json());
 
 // ROUTES
