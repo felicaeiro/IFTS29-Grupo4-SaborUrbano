5564c5b5652b11c8f11f0ac52b63be5643aa9d52
Ian Vázquez <cam.iva@hotmail.com>
2025-10-25 23:24:28 -0300

    ticket

--- routes/pedidoRoutes.js
+++ routes/pedidoRoutes.js
@@ -20,6 +20,7 @@ router.get("/nuevo", async (req, res) => {
     res.render("nuevoPedido", { productos, clientes });
 });
 
+
 // Formulario para editar pedido existente
 router.get("/editar/:id", async (req, res) => {
     const id = req.params.id;
@@ -30,6 +31,19 @@ router.get("/editar/:id", async (req, res) => {
     if (!pedido) return res.status(404).send("Pedido no encontrado");
 
     res.render("editarPedido", { pedido, productos, clientes });
+});
+
+
+// Ticket del pedido
+router.get("/ticket/:id", async (req, res) => {
+    const id = req.params.id;
+    const pedido = await PedidoRepositorio.getPedidoById(id);
+    const productos = await ProductoRepositorio.getProductos();
+    const clientes = await ClienteRepositorio.getClientes();
+    
+    if (!pedido) return res.status(404).send("Pedido no encontrado");
+
+    res.render("ticketPedido", { pedido, productos, clientes });
 });
 
 // CRUD API
--- services/PedidoRepositorio.js
+++ services/PedidoRepositorio.js
@@ -1,26 +1,28 @@
-const Pedido = require('../models/Pedido');
+const Pedido = require("../models/Pedido");
 
 class PedidoRepositorio {
-    async getPedidos() {
-        return await Pedido.find({});
-    }
+  async getPedidos() {
+    return await Pedido.find({});
+  }
 
-    async getPedidoById(id) {
-        return await Pedido.findById(id);
-    }
+  async getPedidoById(id) {
+    return await Pedido.findById(id)
+      .populate("id_cliente")
+      .populate("productos"); 
+  }
 
-    async createPedido(pedidoData) {
-        const newPedido = new Pedido(pedidoData);
-        return await newPedido.save();
-    }
+  async createPedido(pedidoData) {
+    const newPedido = new Pedido(pedidoData);
+    return await newPedido.save();
+  }
 
-    async updatePedido(id, pedidoData) {
-        return await Pedido.findByIdAndUpdate(id, pedidoData, {new: true});
-    }
+  async updatePedido(id, pedidoData) {
+    return await Pedido.findByIdAndUpdate(id, pedidoData, { new: true });
+  }
 
-    async deletePedido(id) {
-        return await Pedido.findByIdAndDelete(id);
-    }
+  async deletePedido(id) {
+    return await Pedido.findByIdAndDelete(id);
+  }
 }
 
 module.exports = new PedidoRepositorio();
